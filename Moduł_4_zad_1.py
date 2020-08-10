109de1e15d334deceab122c19ab23a879ca23768
Kus-Paw <68556631+Kus-Paw@users.noreply.github.com>
2020-08-10 16:57:05 +0200

    second comit

--- Moduł_4_zad_1.py
+++ Moduł_4_zad_1.py
@@ -1,12 +1,33 @@
-#Niestety nie portafie uzyc funkcji boolean nie wyświetlaja mi sie zadne /
+"""
+# Niestety nie portafie uzyc funkcji boolean nie wyświetlaja mi sie zadne /
 # wartosci, postanowilem użyć funkcji for która idzie po kazdyn znaku zdania'word'/
 # i zwrocic wartosc True lub False
-
-def Polidrom2(word):
+"""
+"""
+def polidrom(word):
     for i in range (len(word)//2):
         if (word[0:]) == word[:: -1 ]:
+            word[:] == word[:: -1]:
+            word == word[::-1]
             return True
         else:
             return False
 
-print(Polidrom2('mam'))
+print(polidrom('mam'))
+"""
+# zmniejszenie wszystkich liter i usunięcie spacji, dodatkowko zamiana ',' i '.' na ''.
+
+def polidrom(word):
+    word = word.lower()
+    word = word.replace( ' ', '')
+    word = word.replace(',', '')
+    word = word.replace('.', '')
+
+    for i in range (len(word)//2):
+        if (word[0:]) == word[:: -1 ] and (word[:]) == word[:: -1]:
+            return True
+        else:
+            return False
+
+print(polidrom('Atokiwizdziwi kota'))
+print(polidrom("A to Kamela, ale ma kota."))
